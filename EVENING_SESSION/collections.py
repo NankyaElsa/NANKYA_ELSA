42a809cdc6c88a70ca2026fb86463806c9365bce
NankyaElsa <elsakayigwa@gmail.com>
2024-06-27 16:32:01 +0300

    data analysis with python

--- EVENING_SESSION/collections.py
+++
@@ -1,9 +0,0 @@
-
-#form a dictionary with the names as keys and the number of times they appear in the list the values
-
-people = ["John", "Dave", "Martin", "John", "Brad", "John", "Dave"]
-
-#manually
-names_number = {}
-
-# for name in people:
